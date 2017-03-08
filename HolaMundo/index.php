e9717c159e7dee75a6991dafbd58d2af6008c002
orgui14 <orgui14@INF2-PC04>
2017-03-08 21:16:19 +0100

    segundo cambio: Añadiendo nombres de integrantes.

--- HolaMundo/index.php
+++ HolaMundo/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-               
+              echo "Los integrantes son: 1-Hector Orlando 2-Arnold 3-Stany";
         
         ?>
     </body>
