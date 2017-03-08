a6883a306c627b3a61b7be8bb03b04e62d589573
orgui14 <orgui14@INF2-PC04>
2017-03-08 21:06:45 +0100

    segundo cambio: Añadiendo nombres de integrantes.

--- HolaMundo/index.php
+++ HolaMundo/index.php
@@ -11,7 +11,9 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        echo "1-Hector Orlando Guavita Rojas 2-Arnold 3-Stany";
+        
+        
         ?>
     </body>
 </html>
